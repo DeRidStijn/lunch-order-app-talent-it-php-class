0130bee8eafa99c61f9baeba681a9c0daaa6c87d
unknown <matthijsbraspenning@gmail.com>
2018-03-05 14:39:02 +0100

    Setup Validator

--- Validatie.php
+++ Validatie.php
@@ -1,9 +1,9 @@
 <?php
 
-class Validatie
+/*class Validatie
 {
-	/*protected $filters;
-	protected $validators;*/
+	protected $filters;
+	protected $validators;
 	protected $strings;
 	protected $numbers;
 	public $errorMessage = '';
@@ -37,7 +37,7 @@ class Validatie
 			echo $number;
 		}
 	}
-}
+}*/
 
 /*header("Location: index.php");
 */
@@ -50,4 +50,80 @@ $ToValidate = new Validatie($toValidateStrings, $toValidateNumbers);
 echo $ToValidate->isString();
 echo '<br>';
 echo $ToValidate->isNumber();
-/*echo $ToValidate->errorMessage;*/*/
+/*echo $ToValidate->errorMessage;*/
+
+class orderValidator {
+
+	protected $order;
+	protected $errors:
+
+	public function __construct($order)
+	{
+		$this->order = $order;
+		$this->errors = [];
+	}
+
+	public function isValid(): bool
+	{
+		$validators = [
+			'Aantal' => function($aantal) {
+				if(!is_int($aantal) && !ctype_digit($aantal)) {
+					$this->errors['aantal_digit'] = 'Aantal is geen getal';
+					return false;
+				}
+				if(($aantal < 0) || ($aantal > 1000)) {
+					$this->errors['aantal-range'] = 'Aantal mag niet kleiner zijn dan 0 of groter dan 1000';
+					return false;
+				}
+				return true;
+			},
+			'Grootte' => function($grootte)	{
+				if(!is_string($grootte) && !ctype_alpha($grootte)) {
+					$this->errors['grootte'] = 'Foutieve waarde voor \'Grootte\'';
+					return false;
+				}
+				return true;
+			},
+			'Smos' => function($smos) {
+				if(!is_bool($smos)) {
+					$this->errors['smos'] = 'Geen optie gekozen voor \'Smos\'';
+					return false;
+				}
+				return true;	
+			},
+			'Fitness' => function($fitness) {
+				if(!is_bool($fitness)) {
+					$this->errors['fitness'] = 'Geen optie gekozen voor \'Fitness\'';
+					return false;
+				}
+				return true;
+			},
+			'Type' => function($type) {
+				if(!is_string($type) || '' === $type) {
+					$this->errors['type'] = 'Foutieve waarde voor \'Type\'';
+					return false;
+				}
+				return true;
+			},
+			'Naam' => function($naam) {
+				if(!is_string($naam) || '' === $naam) {
+					$this->errors['naam'] = 'Geef uw naam op aub.';
+					return false;
+				}
+				return true;
+			},
+			'Soep' => function($soep) {
+				if(!is_bool($soep)) {
+					$this->errors['soep'] = 'Geen optie gekozen voor \'soep van de dag\'';
+					return false;
+				}
+				return true;
+			}
+		];
+	}
+
+	public function getErrors(): array 
+	{
+		return $this->errors;
+	}	
+}
