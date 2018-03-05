0941563de5575fede75454b363042268a7bc32e4
De Ridder Stijn <deridder.stijn93@gmail.com>
2018-03-05 14:56:28 +0100

    overbodige functies bleef geen overbodige functies

--- Sessionhandler.php
+++ Sessionhandler.php
@@ -1,10 +1,22 @@
 <?php
 class Sessionhandler{
 
-    function __construct(string $sessionId)
+    function __construct(string $sessionId) //nieuwe persoon aanmaken 
     {
         session_id($sessionId);
     }
 
-} 
+    public function fillBestelling(bool $isSmos, bool $isFitness, string $typeBeleg, bool $isBaguette, string $naam)
+    {
+        $_SESSION['bestelling']['isSmos'] = $isSmos;    // is dit nodig?
+        $_SESSION['bestelling']['isFitness'] = $isFitness;
+        $_SESSION['bestelling']['typeBeleg'] = $typeBeleg;
+        $_SESSION['bestelling']['isBaguette'] = $isBaguette;
+        $_SESSION['bestelling']['naam'] = $naam;
+        header('index.php');
+    }
 
+} 
+/*
+    sessie voor bestelde broodjes + gegevens persoon
+*/
