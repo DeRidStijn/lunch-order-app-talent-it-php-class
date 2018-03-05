448b45bfd0c33b299731080317cb07f081b867bc
De Ridder Stijn <deridder.stijn93@gmail.com>
2018-03-05 14:54:26 +0100

    overbodige functie sessionhandler verwijderd

--- Sessionhandler.php
+++ Sessionhandler.php
@@ -1,24 +1,10 @@
 <?php
 class Sessionhandler{
 
-    function __construct(string $sessionId) //nieuwe persoon aanmaken 
+    function __construct(string $sessionId)
     {
         session_id($sessionId);
     }
 
-    public function fillBestelling(bool $isSmos, bool $isFitness, string $typeBeleg, bool $isBaguette, string $naam)
-    {
-        $_SESSION['bestelling']['isSmos'] = $isSmos;
-        $_SESSION['bestelling']['isFitness'] = $isFitness;
-        $_SESSION['bestelling']['typeBeleg'] = $typeBeleg;
-        $_SESSION['bestelling']['isBaguette'] = $isBaguette;
-        $_SESSION['bestelling']['naam'] = $naam;
-        header('index.php');
-    }
-
-
-
 } 
-/*
-    sessie voor bestelde broodjes + gegevens persoon
-*/
+
