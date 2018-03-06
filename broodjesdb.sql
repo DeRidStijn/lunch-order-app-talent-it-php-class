5b406f1f9f4dff048c30bcf2b549ffef699d7289
Mathieu <mathieu.brusselairs@gmail.com>
2018-03-06 17:19:52 +0100

    DB V1.2.0

--- broodjesdb.sql
+++ broodjesdb.sql
@@ -10,28 +10,36 @@ USE `broodjesapp`;
 
 DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order` (
- `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
- `datum` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
- `soep` TINYINT UNSIGNED DEFAULT 0,
- `broodje_id` INT UNSIGNED NOT NULL,
- `soep_id` INT UNSIGNED NOT NULL,
- `isGroot` TINYINT UNSIGNED DEFAULT 0,
- `isWit` TINYINT UNSIGNED DEFAULT 0,
- PRIMARY KEY (`id`)
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` VARCHAR(30) NOT NULL DEFAULT '',
+    `datum` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `soep` TINYINT UNSIGNED DEFAULT 0,
+    `soepbrood_wit` TINYINT UNSIGNED DEFAULT 0,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+DROP TABLE IF EXISTS `beleg`;
+CREATE TABLE `beleg` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `prijs_klein` DECIMAL(5,2) DEFAULT 0.0,
+    `prijs_groot` DECIMAL(5,2) DEFAULT 0.0,
+    `beleg` VARCHAR(30) NOT NULL DEFAULT '',
+    `omschrijving` VARCHAR(100) NOT NULL DEFAULT '',
+    PRIMARY KEY(`id`)
 ) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
 DROP TABLE IF EXISTS `soep`;
 CREATE TABLE `soep` (
- `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
- `dag` VARCHAR(30) NOT NULL DEFAULT '',
- `soep` VARCHAR(30) NOT NULL DEFAULT '',
- PRIMARY KEY (`id`)
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `soep` VARCHAR(30) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
 DROP TABLE IF EXISTS `order_broodje`;
 CREATE TABLE `order_broodje` (
     `broodje_id` INT UNSIGNED NOT NULL,
     `order_id` INT UNSIGNED NOT NULL,
+    `aantal` INT UNSIGNED NOT NULL,
     PRIMARY KEY (`broodje_id`, `order_id`)
 ) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
@@ -39,8 +47,10 @@ CREATE TABLE `order_broodje` (
 DROP TABLE IF EXISTS `broodje`;
 CREATE TABLE `broodje` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `isWit` TINYINT UNSIGNED DEFAULT 0,
-    `prijs` DECIMAL(5,2) DEFAULT 0.0,
+    `beleg_id` INT NOT NULL DEFAULT 0,
+    `is_wit` TINYINT UNSIGNED DEFAULT 0,
+    `is_groot` TINYINT UNSIGNED DEFAULT 0,
+    `opmerking` VARCHAR(100) NOT NULL DEFAULT '',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
@@ -50,14 +60,17 @@ CREATE TABLE `user` (
     `username` VARCHAR(30) NOT NULL DEFAULT '',
     `naam` VARCHAR(30) NOT NULL DEFAULT '',
     `voornaam` VARCHAR(30) NOT NULL DEFAULT '',
+    `password` VARCHAR(30) NOT NULL DEFAULT '',
     `email` VARCHAR(30) NOT NULL DEFAULT '',
     `potje` INT UNSIGNED NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
+    `is_admin` TINYINT UNSIGNED DEFAULT 0,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
 DROP TABLE IF EXISTS `supplementen`;
 CREATE TABLE `supplementen` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `supplement` VARCHAR(30) NOT NULL,
+    `supplement` VARCHAR(30) NOT NULL DEFAULT '',
+    `prijs` DECIMAL(5,2) NOT NULL DEFAULT 0.0,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
