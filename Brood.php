2128598ac90f92cb0129277ffabe1df7d1092745
unknown <woutergorisit@gmail.com>
2018-03-12 10:00:16 +0100

    type change supplement, int want uit database

--- Brood.php
+++ Brood.php
@@ -11,7 +11,7 @@ class Brood
     protected $opmerking;
     protected $supplement;
 
-    function __construct(bool $smos, bool $fitness, int $typeBeleg, bool $baguette, int $aantalBroodjes, string $opmerking, string $supplement)
+    function __construct(bool $smos, bool $fitness, int $typeBeleg, bool $baguette, int $aantalBroodjes, string $opmerking, int $supplement)
     {
         $this->smos = $smos;
         $this->fitness = $fitness;
@@ -53,7 +53,8 @@ class Brood
         return $this->opmerking;
     }
 
-    function getSupplement() : string {
+    function getSupplement() : int
+    {
         return $this->supplement;
     }
 }
